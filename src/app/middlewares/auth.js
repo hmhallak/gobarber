98453b1698ca7cc8cf699fb52556a6d3dcef6c17
Hussein Mohamad Hallak <hu100wtf@gmail.com>
2019-08-22 23:52:07 -0400

    Adiciona funcionalidades de criação e edição de usuários e sessão

---
+++ src/app/middlewares/auth.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util'; // transforma funções callback em formato de async
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Invalid Token' });
+  }
+
+  return next();
+};
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '37fa9165b41b4ca3e1876fa141ac102e',
+  expiresIn: '7d',
+};
